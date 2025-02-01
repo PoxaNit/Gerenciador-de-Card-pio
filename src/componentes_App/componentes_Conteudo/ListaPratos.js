8cd61e3ea3d6abad8ccc7a68f7b77ed4fae3447a
Júlio <jc.5047792@gmail.com>
2025-02-01 00:37:53 -0400

    Ajustando erros

--- src/componentes_App/componentes_Conteudo/ListaPratos.js
+++ src/componentes_App/componentes_Conteudo/ListaPratos.js
@@ -355,7 +355,7 @@ export default function ListaPratos ({setLista}) {
  {(pratos.sucesso && modoPesquisarPratos && termoPesquisado) && (<ul>
 
  {pratosFiltrados.map((prato, indice) => {
-
+<li>teste</li>
      <ItemDeLista nomeDoPrato={prato.nome_prato} imagemDoPrato={"/" + prato.imagem_prato} descricaoImagem={"Imagem do prato"} indiceParaItem={indice} prato={prato} />
 
  })
