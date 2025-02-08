ab33c72b52c7e787fe9a29b846557dfe776ca50e
Júlio <jc.5047792@gmail.com>
2025-02-07 23:21:29 -0400

    salvando

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from "react";
 import Conteudo from "./componentes_App/Conteudo.js";
+import Autenticado from "./componentes_App/Autenticado.js"; //Contexto de autenticação
 
 function App() {
-  return (
-    <div className="App">
-	<Conteudo />
-    </div>
-  );
+
+    const [autenticado, setAutenticado] = React.useState(false);
+    const [session_id, setSession_id] = React.useState("");
+
+    async function verificarAutenticacao() {
+
+        const autenticacao = await fetch("/autenticacao/session_id.php").then(resposta => resposta.json());
+
+        if (!autenticacao.sucesso) {
+
+            window.location = "http://0.0.0.0:8000/autenticacao/login.php";
+
+        } else {
+
+            setAutenticado(autenticacao.sucesso);
+	    setSession_id(autenticacao.id);
+
+        };
+
+    };
+
+    React.useEffect(() => {
+
+        (function () {
+
+	     verificarAutenticacao();
+
+        })();
+
+    },[]);
+
+
+    return autenticado && (
+        <Autenticado.Provider value={{autenticado, session_id}}>
+            <div className="App">
+   	        <Conteudo />
+            </div>
+        </Autenticado.Provider>
+    );
 }
 
 export default App;
---
+++ src/componentes_App/Autenticado.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+ const Autenticado = React.createContext();
+
+ export default Autenticado;
