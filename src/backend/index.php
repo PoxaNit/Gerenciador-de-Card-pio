ddab98f931d6c43441c2db2730a6017c4478ca19
Júlio <jc.5047792@gmail.com>
2025-05-01 17:24:06 -0400

    Ajustes

--- src/backend/index.php
+++ src/backend/index.php
@@ -1,19 +1,19 @@
 <?php
 
 
- if ($_SERVER["REQUEST_URI"] === "/retornar_dados"):
+ if ($_SERVER["REQUEST_URI"] === "http://gerenciadormenu.free.nf/retornar_dados"):
 	require_once "retornar_dados.php";
  exit;
 
-   elseif ($_SERVER["REQUEST_URI"] === "/criar_prato"):
+   elseif ($_SERVER["REQUEST_URI"] === "http://gerenciadormenu.free.nf/criar_prato"):
 	include "criar_prato.php";
  exit;
 
-   elseif ($_SERVER["REQUEST_URI"] === "/deletarPrato"):
+   elseif ($_SERVER["REQUEST_URI"] === "http://gerenciadormenu.free.nf/deletarPrato"):
 	include "deletarPrato.php";
  exit;
 
-   elseif ($_SERVER["REQUEST_URI"] === "/atualizarPrato"):
+   elseif ($_SERVER["REQUEST_URI"] === "http://gerenciadormenu.free.nf/atualizarPrato"):
 	include "atualizarPrato.php";
  exit;
 
